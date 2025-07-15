6a84945b9e9d24c27e814372515f2501adf7cb6f
Kenneth Andrade <kennethandradeom@gmail.com>
2025-07-14 21:58:56 -0500

    Actualizacion unicamente funcionamiento anterior

--- script.js
+++ script.js
@@ -4,41 +4,12 @@ const input = document.getElementById('taskInput');
 const boton = document.getElementById('addTaskButton');
 const taskList = document.getElementById('taskList');
 
-function mostrarTarea(tarea) {
-    const li = document.createElement('li');
-    li.className = "bg-white p-3 rounded shadow flex items-center space-x-3 hover:bg-gray-100 cursor-pointer";
-
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = tarea.getCompletada();
-    checkbox.className = "form-checkbox h-5 w-5 text-blue-600";
-
-    checkbox.addEventListener('change', () => {
-        tarea.setCompletada(checkbox.checked);
-        guardarTareas();
-
-        if (checkbox.checked) {
-            span.style.textDecoration = "line-through";
-            span.style.color = "gray";
-        } else {
-            span.style.textDecoration = "none";
-            span.style.color = "black";
-        }
-    });
-
-    const span = document.createElement('span');
-    span.textContent = tarea.getTexto();
-
-    if (tarea.getCompletada()) {
-        span.style.textDecoration = "line-through";
-        span.style.color = "gray";
-    }
-
-    li.appendChild(checkbox);
-    li.appendChild(span);
+function mostrarTarea(tarea){
+    const li= document.createElement('li');
+    li.textContent = tarea.getTexto()
+    li.className = "bg-white p-3 rounded shadow hover:bg-gray-100 cursor-pointer";
     taskList.appendChild(li);
 }
-
 
 function guardarTareas(){
     localStorage.setItem("tareas", JSON.stringify(tareas));
